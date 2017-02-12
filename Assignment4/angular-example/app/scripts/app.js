9f8f394625fc6013a1e6ced0b743770de6da4a46
ginkgomp <lvyuopo@hotmail.com>
2017-02-12 17:34:36 -0500

    Using Yeoman

---
+++ Assignment4/angular-example/app/scripts/app.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularExampleApp
+ * @description
+ * # angularExampleApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularExampleApp', [
+    'ngResource',
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
